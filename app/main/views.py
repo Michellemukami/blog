8742d505aaab66eebb0f39043b48ed876da4875f
Michelle Mukami <you@example.com>
2019-08-09 08:39:05 +0300

    installed reqirements

--- app/main/views.py
+++ app/main/views.py
@@ -115,3 +115,32 @@ def profile(uname):
 
     return render_template("profile/profile.html", user = user)
 
+@main.route("/post/<int:post_id>/update", methods= ['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Pitch.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated', 'success')
+        return redirect(url_for('posts.post', post_id =  post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html',title = 'Update Post', form = form, legend = 'Update Post')
+
+
+@main.route("/post/<int:post_id>/delete", methods= ['GET','POST'])
+@login_required
+def delete_post(post_id):
+    post = Pitch.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('main.home'))
