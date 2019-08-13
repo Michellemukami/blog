a61b0ee8d43d61dbc2c1513ef6da54ac162d812e
Michelle Mukami <you@example.com>
2019-08-13 07:53:14 +0300

    added changes to congfig .py

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ from  flask_migrate import Migrate, MigrateCommand
 # Creating app instance
 
 app = create_app('production')
+
 manager = Manager(app)
 
 manager.add_command('server',Server)
@@ -14,7 +15,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User, Pitch=Pitch,Blog=Blog)
+    return dict(app = app,db = db,User = User,Blog=Blog)
 
 if __name__ == '__main__':
     manager.run()
