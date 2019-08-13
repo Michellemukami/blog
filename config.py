1441f5c8fbedfa928b1d0fc3e24c4c54d20748cd
Michelle Mukami <you@example.com>
2019-08-13 11:18:15 +0300

    deployment

--- config.py
+++ config.py
@@ -3,20 +3,20 @@ import os
 class Config:
 
    
-    SECRET_KEY = 'kamikaze'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michelle:kami@localhost/blogger'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     SQLALCHEMY_TRACK_MODIFICATIONS=True
-    MAIL_USERNAME = "michellemukami.g@gmail.com"
-    MAIL_PASSWORD = "kamikaze@99"
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
     QUOTES_API='http://quotes.stormconsultancy.co.uk/random.json'
 class ProdConfig(Config):
-     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michelle:kami@localhost/blog'
+     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michelle:kami@localhost/blogs_test'
    
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 export SECRET_KEY='kamikaze'
 export MAIL_USERNAME='michellemukami.g@gmail.com'
 export MAIL_PASSWORD='kamikaze@99'
-
+export DATABASE_URL = 'postgresql+psycopg2://michelle:kami@localhost/blog'
 python3.6 manage.py server
 
 
