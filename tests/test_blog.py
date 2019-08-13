2571f6697c8c5d339c2766681f22031a6d4882b8
Michelle Mukami <you@example.com>
2019-08-13 08:15:51 +0300

    changes made in tests

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -1,22 +1,22 @@
 import unittest
-from app.models import Pitch, User
+from app.models import Blog, User
 from app import db
 
-class PitchTest(unittest.TestCase):
+class BlogTest(unittest.TestCase):
 
     def setUp(self):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_pitch = Pitch("This is a new pitch")
+        self.new_Blog = Blog("This is a new pitch")
 
 
     def test_check_instance_variables(self):
-        self.assertEquals(self.new_pitch.comments,"This is a new pitch")
+        self.assertEquals(self.new_Blog.comments,"This is a new pitch")
       
     def test_save_pitch(self):
         """
         To tests on whether pitches are being saved
         """
-        self.new_pitch.save_pitch()
-        self.assertTrue(len(Pitch.query.all())>0)
+        self.new_blog.save_blog()
+        self.assertTrue(len(Blog.query.all())>0)
