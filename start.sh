2710fb6d19fed50243e9302f1c58111f2cd42b10
Michelle Mukami <you@example.com>
2019-08-13 07:59:19 +0300

    added secret_key

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 
 export SECRET_KEY='kamikaze'
 export MAIL_USERNAME='michellemukami.g@gmail.com'
-export MAIL_PASSWORD='kamikaze@99'
+export MAIL_PASSWORD='kamikae@99'
 
 python3.6 manage.py server
 
